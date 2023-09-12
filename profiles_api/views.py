ad19e835da00ae3ac41e723f482b928a13b15a25
Rohail Najam <rohail.najam@oxit.com>
2023-09-12 17:48:55 +0500

    Added Post, Put, Patch and Delete Method.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,13 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from profiles_api.serializers import HelloSerializer
 
 
 class HelloApiView(APIView):
     """ Test API View"""
+
+    serializer_class = HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView Features """
@@ -15,4 +18,34 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs'
         ]
         return Response({'message': 'Helloo !!', 'data': an_apiview},
+                        status=status.HTTP_200_OK)
+
+
+    def post(self, request):
+        """Create a Hello Message with out Name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            return Response({'message': f'Helloo {name}!!'},
+                            status=status.HTTP_200_OK)
+        else:
+            return Response({'errors': serializer.errors},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        """Handling Update to an object"""
+        return Response({'method': "This is Put Request"},
+                        status=status.HTTP_200_OK)
+
+    def patch(self, request, pk=None):
+        """Handling partial update to an object"""
+        return Response({'method': "This is PATCH Request"},
+                        status=status.HTTP_200_OK)
+
+
+    def delete(self, request, pk=None):
+        """Handling Delete to an object"""
+        return Response({'method': "This is Delete Request"},
                         status=status.HTTP_200_OK)
