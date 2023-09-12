b3985937dd1f725d12e2ada73185377ddea7ef84
Rohail Najam <rohail.najam@oxit.com>
2023-09-12 17:19:55 +0500

    Enabled Django Admin.

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -12,7 +12,7 @@ class UserProfileManager(BaseUserManager):
             raise ValueError("User must have a vaild email address")
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
-        user.set_password(password=password)
+        user.set_password(password)
         user.save(using=self._db)
 
         return user
