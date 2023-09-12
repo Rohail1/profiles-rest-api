de65b1e3b8893c5427b22782adda36e53f879917
Rohail Najam <rohail.najam@oxit.com>
2023-09-12 17:33:26 +0500

    Created Basic API View.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """ Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView Features """
+        an_apiview = [
+            "Uses HTTP Methods as a function (get, post, patch, put, delete)",
+            "Is similar to a traditional django view",
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs'
+        ]
+        return Response({'message': 'Helloo !!', 'data': an_apiview},
+                        status=status.HTTP_200_OK)
